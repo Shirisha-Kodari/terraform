d6a4a6c9863447554210d6d4c9b990073a17c23b
SHIRISHA KODARI <shirishakodari35@gmail.com>
2026-04-02 17:22:53 +0530

    Update locals.tf

--- locals/locals.tf
+++ locals/locals.tf
@@ -2,7 +2,7 @@ locals {
   final_name = "${var.project}-${var.environment}-${var.component}" #roboshop-dev-cart
   ec2_tags = merge(
     var.common_tags,
-    {
+    { 
         environment = "dev", 
         version = "1.0" 
     }
